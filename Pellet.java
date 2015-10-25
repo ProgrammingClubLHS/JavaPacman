327f08b1a44d3966abc521fabad8c055391f7013
David Arico <davidarico@me.com>
2015-10-24 22:41:51 -0700

    Update Pellet.java

--- Pellet.java
+++ Pellet.java
@@ -1,1 +1,25 @@
+import greenfoot.*;
 
+
+/**
+ * Write a description of class WALL here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Wall extends Actor
+{
+    public Wall(){
+        super();
+        getImage().scale(10,10);
+    }
+    
+    /**
+     * Act - do whatever the WALL wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       
+    }    
+}
