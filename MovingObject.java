89c25d4ce229b62f580b6e1a0cf67601249b2cc4
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-10-24 21:12:29 -0700

    Copied from greenfoot
    We don't know how to use the command line yet.

---
+++ MovingObject.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class MovingObject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MovingObject extends Actor
+{
+    /**
+     * Act - do whatever the MovingObject wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    protected boolean wallInFront(){
+        Object[] intersectingThings = getIntersectingObjects(Wall.class).toArray();
+        boolean wallPresence = false;
+        if(intersectingThings != null && intersectingThings.length > 0){
+            for(int index = 0; index < intersectingThings.length; index++){
+                Wall possibleWall = (Wall)(intersectingThings[index]);
+                if      (possibleWall.getX() > getX() && getRotation() == 0){
+                    wallPresence = true;
+                }else if(possibleWall.getX() < getX() && getRotation() == 180){
+                    wallPresence = true;
+                }else if(possibleWall.getY() > getY() && getRotation() == 90){
+                     wallPresence = true;
+                }else if(possibleWall.getY() < getY() && getRotation() == 270){
+                     wallPresence = true;
+                }
+            }
+        }
+        return wallPresence;
+    }
+    public void act() 
+    {
+        for(int i = 0;i < 3; i++){
+            try{
+                if(!wallInFront()){
+                    move(1);
+                }
+            }catch( RuntimeException e){
+            Greenfoot.ask(e.getMessage());
+            }
+        }
+        if(Greenfoot.isKeyDown(" ")){
+            move(3);
+        }
+    }    
+}
