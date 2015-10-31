080203f4f82a0629d221b29f2444e6481f3b5835
Alex/sprocket <Alexhb61@users.noreply.github.com>
2015-10-30 22:13:40 -0700

    Added image code

--- GhostWall.java
+++ GhostWall.java
@@ -1,4 +1,11 @@
 // Wall that ghosts can go through but Pacman can't
 public class GhostWall extends Wall{
-  
+    public GhostWall(){
+        super();
+        getImage().scale(Wall.SIZE,Wall.SIZE /2);
+    
+    }
+    public void act(){
+
+    }     
 }
