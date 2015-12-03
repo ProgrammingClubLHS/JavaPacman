e893886dad8390947b897dd8d680ea4fc243b642
Cameron <camm73@yahoo.com>
2015-12-02 22:24:31 -0800

    Added the pacman mouth animation

--- Pacman.java
+++ Pacman.java
@@ -15,15 +15,69 @@ public class Pacman extends MovingObject
     //My change line : in soviet russia, revolution starts you.
     //THE COMMUNIST REVOLUTION STARTS WITH YOU
     private int score;
+    private final GreenfootImage pacmanOpen = new GreenfootImage("Original_PacMan (1).png");
+    private final GreenfootImage pacmanClosed = new GreenfootImage("closedPacman.png"); //TODO make this image
+    int anim = 0;
+    private boolean moving = true;
     public Pacman(){
         super();
         getImage().scale(Wall.SIZE, Wall.SIZE);
+        pacmanOpen.scale(Wall.SIZE, Wall.SIZE);
+        pacmanClosed.scale(Wall.SIZE, Wall.SIZE);
         score = 0;
     }
     public int getScore(){
         return score;
     }
+    
+    public void animate(){
+           if(anim > 1000){
+              anim = 0;
+            }
+       
+            
+            if((anim % 2) == 0 && getMoving()){
+                setImage(pacmanClosed);
+            }else{
+                setImage(pacmanOpen);
+            }
+        anim++;
+    }
+    
+    private boolean getMoving(){
+         if(getRotation()==0){
+            if(!wallInFront(0)){
+                moving = true;
+            }else{
+                moving = false;
+            }
+        }else if(getRotation()==90){
+            if(!wallInFront(90)){
+                moving = true;
+            }else{
+                moving = false;
+            }
+        }else if(getRotation()==180){
+            if(!wallInFront(180)){
+                moving = true;
+            }else{
+                moving = false;
+            }
+        }else if(getRotation()==270){
+            if(!wallInFront(270)){
+                moving = true;
+            }else{
+                moving = false;
+            }
+        }else{
+            moving = false;
+        }
+        
+        return moving;
+    }
+    
     public void act(){
+        animate();
         if(Greenfoot.isKeyDown("d") && !wallInFront(0)){
             setRotation(0);
         }else if(Greenfoot.isKeyDown("a") && !wallInFront(180)){
@@ -31,7 +85,7 @@ public class Pacman extends MovingObject
         }else if(Greenfoot.isKeyDown("s") && !wallInFront(90)){
             setRotation(90);
         }else if(Greenfoot.isKeyDown("w") && !wallInFront(270)){
-            setRotation(270);
+           setRotation(270);
         }
         if(getX() == 0 && getRotation() == 180){
             setLocation(getWorld().getWidth(),getY());
